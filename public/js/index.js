1292364bbfc8f4e70b9942da497a846a8c5fe8a5
Vikrant Mahajan <vikrantm93@gmail.com>
2018-08-05 14:17:33 +0530

    add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -30,11 +30,13 @@ socket.on('newLocationMessage',function(message){
 jQuery('#message-form').on('submit', function(e){
 	e.preventDefault();
 
+	var messageTextbox = jQuery('[name=message]');
+
 	socket.emit('createMessage',{
 		from:'User',
-		text : jQuery('[name=message]').val()
+		text : messageTextbox.val()
 	},function(){
-
+		messageTextbox.val('')
 	});
 });
 
@@ -44,13 +46,17 @@ locationButton.on('click',function(){
 		return alert('Geolocation not supported by your browser.');
 	}
 
+	locationButton.attr('disabled','disabled').text('Sending location..');
+
 	navigator.geolocation.getCurrentPosition(function(position){
 		console.log(position);
+		locationButton.removeAttr('disabled').text('Send Location');
 		socket.emit('createLocationMessage',{
 			latitude: position.coords.latitude,
 			longitude: position.coords.longitude
 		});
 	},function(){
+		locationButton.removeAttr('disabled').text('Send Location');
 		alert('Unable to fetch location.');
 	});
 });
